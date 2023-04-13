67df2b7a71d5f3aab0e00a06751c70bec10d6d02
Ashutosh Mishra <91246284+Ashutosh-Mishra01@users.noreply.github.com>
2023-04-14 02:43:25 +0530

    Time: 738 ms (18.70%), Space: 52.1 MB (10.55%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int,bool> present;
+        for(int x:nums){
+            present[x]=true;
+        }
+        int maxlen=0;
+        for(int z:nums){
+            if(!present[z-1]){
+                int currlen=0;
+                while(present[z++]){
+                    currlen++;
+                }
+                maxlen=max(currlen,maxlen);
+            }
+        }
+        return maxlen;
+        
+        // for(auto x:present){
+        //     if(!present[x.first-1]){
+        //         int currlen=0;
+        //         int start=x.first;
+        //         while(present[start]){
+        //             currlen++;
+        //             start++;
+        //         }
+        //         if(currlen>maxlen) maxlen=currlen;
+        //     }
+        // }
+        // return maxlen;
+
+    }
+};
